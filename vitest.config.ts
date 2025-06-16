5d691853d35df859dd97996f5ba32688dcb2f9d9
Ivan Roman <DigitalHerencia@Outlook.com>
2025-06-16 05:20:32 -0600

    test: add dispatch domain tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,14 @@
 /** @format */
 
 import { defineConfig } from "vitest/config"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
     test: {
         environment: "node",
         globals: true,
+        include: ["tests/**/*.test.ts"],
+        exclude: ["tests/e2e/**"],
     },
+    plugins: [tsconfigPaths()],
 })
