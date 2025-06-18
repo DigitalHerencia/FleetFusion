2f940774bcbf502ed0d7b4c5bfa13ab0fcdc2492
DigitalHerencia <digitalherencia@outlook.com>
2025-06-18 13:48:52 -0600

    ...

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,7 @@
 /** @format */
 
-import { defineConfig } from "vitest/config"
 import tsconfigPaths from "vite-tsconfig-paths"
+import { defineConfig } from "vitest/config"
 
 export default defineConfig({
     plugins: [tsconfigPaths()],
@@ -10,8 +10,8 @@ export default defineConfig({
         exclude: ["tests/e2e/**", "node_modules/**"],
         environment: "node",
         globals: true,
-        threads: true,
-        retry: process.env.CI ? 2 : 0,        coverage: {
+        retry: process.env.CI ? 2 : 0,
+        coverage: {
             provider: "v8",
             reporter: ["text", "html", "json"],
         },
